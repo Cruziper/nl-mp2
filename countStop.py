56b23f9190f1aa49e298b360dc4249bbd31d9854
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2022-10-23 22:26:34 +0100

    Initial trial with stop word count in the dev set

--- countStop.py
+++ countStop.py
@@ -2,13 +2,5 @@
 file = open("stopwords.txt", "r")
 string = file.read()
 fixedSTR = string.replace("\"", "\'")
-print(fixedSTR[0])
-print(fixedSTR[1])
-print(fixedSTR[2])
-print(fixedSTR[3])
 elem = fixedSTR[2:-3]
 elems = elem.split("\', \'")
-print(elems[0])
-print(elems[1])
-print(elems[2])
-print(elems[3])
