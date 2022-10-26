2769a2dad4d56f9e3f1f30d2c5bd55b3e5afbd18
Cruziper <goncalo.garcia.cruz@tecnico.ulisboa.pt>
2022-10-26 11:14:37 +0100

    Tokenization + Lower Casing + Stemming implemented

--- countStop.py
+++ countStop.py
@@ -1,6 +1,38 @@
 ## Read and transform list of stopwords
-file = open("stopwords.txt", "r")
-string = file.read()
+special_characters = "\"!@#$%^&*()-+?_=,<>/\\..." ## Added ...
+stopWordsF = open("stopwords.txt", "r")
+string = stopWordsF.read()
 fixedSTR = string.replace("\"", "\'")
 elem = fixedSTR[2:-3]
-elems = elem.split("\', \'")
+swArray = elem.split("\', \'")
+
+reviewsCorpus = []
+with open("dev.txt", "r") as devF:
+    for line in devF:
+        reviewsCorpus.append(line.split("\t")[1])
+        
+reviewsSplit = []
+reviewWordsLC = []
+for review in reviewsCorpus:
+    reviewsSplit.append(review.split(" "))
+for review in reviewsSplit:
+    for word in review:
+        if word not in special_characters:
+            reviewWordsLC.append(word.lower())
+
+print(reviewWordsLC)
+
+countWords = []
+for stopWord in swArray:
+    countWords.append(reviewWordsLC.count(stopWord))
+
+print(countWords)
+
+important_words = []
+for index in range(len(countWords)):
+    if countWords[index] == 1:
+        important_words.append(swArray[index])
+print("\n\n")
+print(swArray)
+print("\n")
+print(important_words)
--- model.py
+++ model.py
@@ -1,3 +1,67 @@
 import nltk
 from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+
 print(stopwords.words('english'))
+
+porter = PorterStemmer()
+lancaster = LancasterStemmer()
+
+ranks = []
+reviews = []
+
+with open("dev.txt", "r") as devF:
+    for line in devF:
+        rank_review = line.split("\t")
+        ranks.append(rank_review[0])
+        reviews.append(rank_review[1])
+
+print(ranks)
+print(reviews)
+
+## Tokenized and lower case review words
+reviewsToken = [] # stores all reviews tokenized
+for review in reviews:
+    reviewsToken.append(review.split(" "))
+
+reviewsTokenLC = [] # stores all reviews tokenized and lower cased
+for review in reviewsToken:
+    tempReview = []
+    for token in review:
+        tempReview.append(token.lower())
+    reviewsTokenLC.append(tempReview)
+
+print("\nREVIEWS TOKENIZED --------------------------------------------------------------------------------")
+print(reviewsToken)
+
+print("\nTOKENS LOWER CASE --------------------------------------------------------------------------------")
+print(reviewsTokenLC)
+
+## Word stemming
+# Porter Stemming - least aggressive
+reviewsTokenLCstemP = [] # stores all reviews tokenized, lower cased and Porter stemmed
+for review in reviewsTokenLC:
+    tempReview = []
+    for token in review:
+        tempReview.append(porter.stem(token))
+    reviewsTokenLCstemP.append(tempReview)
+
+print("\n\nPORTER STEMMED --------------------------------------------------------------------------------")
+print(reviewsTokenLCstemP)
+
+
+# Lancaster Stemming - least aggressive
+reviewsTokenLCstemL = [] # stores all reviews tokenized, lower cased and Lancaster stemmed
+for review in reviewsTokenLC:
+    tempReview = []
+    for token in review:
+        tempReview.append(lancaster.stem(token))
+    reviewsTokenLCstemL.append(tempReview)
+
+print("\n\nLANCASTER STEMMED --------------------------------------------------------------------------------")
+print(reviewsTokenLCstemL)
+
+# Cross Validation - scikit-learn library
+
+# Naive Bayes - scikit-learn library
