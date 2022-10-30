78775b86a134ecdfd4b3930e01de7039f1e3ddab
NunoDamas <56343011+NunoDamas@users.noreply.github.com>
2022-10-30 16:49:41 +0000

    CV - Linear Support Vector Machine

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold, cross_val_score
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn import svm
 
 #print(stopwords.words('english'))
 
@@ -91,6 +92,17 @@ clf = DecisionTreeClassifier(random_state=42)
 k_folds = KFold(n_splits = 5)
 
 scores = cross_val_score(clf, x, y, cv = k_folds)
+
+print("Cross Validation Scores: ", scores)
+print("Average CV Score: ", scores.mean())
+print("Number of CV Scores used in Average: ", len(scores))
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=0)
+
+clf = svm.SVC(kernel='linear', C=1).fit(x_train, y_train)
+clf.score(x_test, y_test)
+
+scores = cross_val_score(clf, x, y, cv=5)
 
 print("Cross Validation Scores: ", scores)
 print("Average CV Score: ", scores.mean())
